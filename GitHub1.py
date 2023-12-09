a0e8b5b41f52e0632870c35718ea14cd07cc0a72
Eber curiel <eber.curiel@uabc.edu.mx>
2023-12-08 20:27:45 -0800

    Eliminado mercado libre y conexion con la base de datos corregida

--- GitHub1.py
+++ GitHub1.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,32 +8,21 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import mysql.connector
+import numpy as np
 
 def conectar():
-    try:
-        conexion = mysql.connector.connect(host="localhost",
-                                           port="3306",
-                                           user="root",
-                                           password="user",
-                                           db="proyecto")
-        if conexion.is_connected():
-            print("Se ha logrado la conexion")
-            informacion = conexion.get_server_info()
-            print(informacion)
-            cursor = conexion.cursor()
-            cursor.execute("SLECT DATABASE()")
-            #Usamos fetchone porque solamente seleccionaremos una bade de datos, proyecto
-            row=cursor.fetchone()
-            print("conectado a:{}".format(row))
-            # seleccionamos
-            cursor.execute("SELECT * FROM tienda")
-
-            # obtenemos todos los resultados
-            tiendas = cursor.fetchall()
-            for i in tiendas:
-                print(i)
-    except Exception as error:
-        print("No se logro completar la conexion")
+    conexion = mysql.connector.connect(
+        host='localhost',
+        port='3306',
+        user='root',
+        password='',
+        database='proyecto')
+    print("Conexion exitosa")
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM tienda")
+    consulta = cursor.fetchall()
+    for i in consulta:
+        print(i)
 
 
 s = Service(ChromeDriverManager().install())
@@ -51,9 +39,15 @@ datos = {
     "Puntuaciones": []
 }
 
-
+dashboard1 = 0
+dashboard2 = 0
+dashboard3 = 0
 def creardf():
     dataframe = pd.DataFrame(datos)
+    dataframe = dataframe[dataframe["Precio"] != "No disponible"]
+    dataframe = dataframe[dataframe["Calificacion"] != "No disponible"]
+    dataframe = dataframe[dataframe["Puntuaciones"] != 0]
+    dataframe["Calificacion"] = dataframe["Calificacion"].apply(lambda x: x[:3])
     print(dataframe)
     dataframe.to_csv("C:/Users/ecuri/Desktop/dataset.csv")
 
@@ -68,7 +62,6 @@ def amazonMx(busqueda, pgs):
     time.sleep(3)
     barra.send_keys(Keys.ENTER)
 
-    #cant_paginas = 2
     for pagina in range(pgs - 1):
         print(pagina)
         contenido_pagina = navegador.page_source
@@ -82,9 +75,6 @@ def amazonMx(busqueda, pgs):
             pais = "Mexico"
             producto = i.find("span", attrs={"class": "a-size-base-plus a-color-base a-text-normal"}).text
             # AGREGUE LOS .TEXT ANTES, PORQUE SI LOS AGREGAS ABAJO MARCA ERROR.
-            # PORQUE EJEMPLO SI NO HAY PRECIO PONES NO DISPONIBLE DIRECTO ESO ES
-            # UNA CADENA POR LO QUE NO TIENE EL ATRIBUTO TEXT.
-            # SI EXISTIERA ENTRA EL TRY, EL FIND REGRESA UN OBJETO QUE TIENE EL ATRIBUTO TEXT
             try:
                 precio = i.find("span", attrs={"class": "a-price-whole"}).text
             except:
@@ -111,68 +101,9 @@ def amazonMx(busqueda, pgs):
             print("No se pudo acceder a la siguiente pagina o no existe una siguiente pagina")
             break
         time.sleep(5)
-    navegador.close()  # <-- LO SAQUE DEL CICLO, PORQUE CIERRA EL NAVEGADOR, ANTES DE ACABAR EL CICLO DE LAS PAGINAS
-
-def mercadoLibre(busqueda, pgs):
-    #testeo para probar barras de busqueda
-    global datos
-    # mandar comando a la barra de busqueda para que escriba el item escrito y hacer el web scrapping desde la pagina que resulte
-    navegador = webdriver.Chrome(service=s, options=opc)
-    navegador.get("https://www.mercadolibre.com.mx/")
-    barra = navegador.find_element(By.ID, value="cb1-edit")
-    time.sleep(2)
-    barra.send_keys(busqueda)
-    time.sleep(3)
-    barra.send_keys(Keys.ENTER)
-    time.sleep(10)
-
-    #cant_paginas = 3
-    for pagina in range(pgs - 1):
-        print(pagina)
-        # con page_source se obtiene el contenido HTML de la pagina actual
-        contenido_pagina = navegador.page_source
-        soup = BeautifulSoup(contenido_pagina, "html.parser")
-        productos = soup.find_all('div', attrs={'class': 'andes-card ui-search-result ui-search-result--core andes-card--flat andes-card--padding-16'})
-        botonSiguiente = navegador.find_element(By.LINK_TEXT, "Siguiente")
-        # i va a equivaler a cada producto disponible en la pagina porque comparten el atributo del soup
-        for i in productos:
-            marca = "Samsung"
-            tienda = "Mercado Libre"
-            pais = "Mexico"
-            producto = i.find("h2", attrs={"class": "ui-search-item__title"}).text
-            # AGREGUE LOS .TEXT ANTES, PORQUE SI LOS AGREGAS ABAJO MARCA ERROR.
-            # PORQUE EJEMPLO SI NO HAY PRECIO PONES NO DISPONIBLE DIRECTO ESO ES
-            # UNA CADENA POR LO QUE NO TIENE EL ATRIBUTO TEXT.
-            # SI EXISTIERA ENTRA EL TRY, EL FIND REGRESA UN OBJETO QUE TIENE EL ATRIBUTO TEXT
-            try:
-                precio = i.find("span", attrs={"class": "andes-money-amount__fraction"}).text
-            except:
-                precio = "No disponible"
-            try:
-                calificacion = i.find('span', attrs={"class": "ui-search-reviews__rating-number"}).text
-            except:
-                calificacion = "No disponible"
-            try:
-                puntuaciones = i.find('span', attrs={"class": "ui-search-reviews__amount"}).text
-            except:
-                puntuaciones = 0
-            datos["Marca"].append(marca)
-            datos["Tienda"].append(tienda)
-            datos["Pais"].append(pais)
-            datos["Producto"].append(producto)
-            datos["Precio"].append(precio)
-            datos["Calificacion"].append(calificacion)
-            datos["Puntuaciones"].append(puntuaciones)
-        try:
-            botonSiguiente.click()
-        except:
-            print("No se pudo acceder a la siguiente pagina o no existe una siguiente pagina")
-            break
-        time.sleep(5)
     navegador.close()
 
 def amazonUS(busqueda, pgs):
-    # testeo para probar barras de busqueda
     global datos
     # mandar comando a la barra de busqueda para que escriba el item escrito y hacer el web scrapping desde la pagina que resulte
     navegador = webdriver.Chrome(service=s, options=opc)
@@ -185,7 +116,6 @@ def amazonUS(busqueda, pgs):
     barra.send_keys(Keys.ENTER)
     time.sleep(10)
 
-    #cant_paginas = 2
     for pagina in range(pgs - 1):
         print(pagina)
         contenido_pagina = navegador.page_source
@@ -197,14 +127,7 @@ def amazonUS(busqueda, pgs):
             marca = "Samsung"
             tienda = "Amazon"
             pais = "USA"
-            #Si no pongo punto text me regresatodo el valor de la etiqueta, si si pongo .text
-            #  me marca error 'NoneType' object has no attribute 'text'
-            #Error
-            #Error
-            #Error
             producto = i.find("span", attrs={"class": "a-size-medium a-color-base a-text-normal"}).text
-            #.TEXT PORQUE SI LOS AGREGAS ABAJO MARCA ERROR.
-            # SI EXISTIERA ENTRA EL TRY, EL FIND REGRESA UN OBJETO QUE TIENE EL ATRIBUTO TEXT
             try:
                 precio = i.find("span", attrs={"class": "a-price-whole"}).text
             except:
@@ -230,7 +153,7 @@ def amazonUS(busqueda, pgs):
             print("No se pudo acceder a la siguiente pagina o no existe una siguiente pagina")
             break
         time.sleep(5)
-    navegador.close()  # <-- LO SAQUE DEL CICLO, PORQUE CIERRA EL NAVEGADOR, ANTES DE ACABAR EL CICLO DE LAS PAGINAS
+    navegador.close()
 
 
 while True:
@@ -240,9 +163,8 @@ while True:
         print("Texto texto texto")
         input(" ")
     elif opcion == 2:
-        os.system('cls')
         print("Webs de donde es posible extraer datos:")
-        web = int(input("\n1. Amazon Mexico. \n2. Mercado Libre. \n3. Amazon de Estados Unidos. \n4. Best Buy \n"))
+        web = int(input("\n1. Amazon Mexico. \n2. Amazon de Estados Unidos.\n "))
         if web == 1:
             pags = int(input("De cuantas paginas quieres extraer los datos (cantidad) "))
             prd = input("Cual es el producto samsung que deseas buscar? solo es necesario el modelo ")
@@ -251,26 +173,20 @@ while True:
         elif web == 2:
             pags = int(input("De cuantas paginas quieres extraer los datos (cantidad) "))
             prd = input("Cual es el producto samsung que deseas buscar? solo es necesario el modelo ")
-            mercadoLibre(prd, pags)
-            input(" ")
-        elif web == 3:
-            pags = int(input("De cuantas paginas quieres extraer los datos (cantidad) "))
-            prd = input("Cual es el producto samsung que deseas buscar? solo es necesario el modelo ")
             amazonUS(prd, pags)
-            input(" ")
-        elif web == 4:
-            pags = int(input("De cuantas paginas quieres extraer los datos (cantidad) "))
-            prd = input("Cual es el producto samsung que deseas buscar? solo es necesario el modelo ")
-            bestBuy(prd, pags)
             input(" ")
         else:
             print("Comando invalido")
     elif opcion == 3:
             creardf()
-            input(" ")
     elif opcion == 4:
-        print("aun nada")
-        pass
+        acc = int(input("Opciones \n1. Acceder a la base de datos. \n2. Subir datos a la base de datos\n"))
+        if acc == 1:
+            conectar()
+        elif acc == 2:
+            pass
+        else:
+            print("Comando invalido")
     elif opcion == 5:
         print("aun nada")
         pass
